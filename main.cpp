a93700835849c192c5b8727ee3c7d56393514cc7
mayra miranda moreno <mayra_190@MacBook-Air-192.local>
2025-10-03 14:37:34 -0600

    Ajuste de CMake y confirmación de archivos para CLion

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "Node.h"
+#include "DoublyLinkedList.h"
+#include<iostream>
+
+int main() {
+    // Tu código de prueba para DoublyLinkedList aquí
+    printf("Hello, World!\n");
+    return 0;
+}
