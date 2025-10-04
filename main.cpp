d017e51b8f0ed45e5aadd4c7a65359fbeec6a2d5
Juan Pa <jpgavidiaa@gmail.com>
2025-10-03 20:11:34 -0600

    Actualización del main: Solo realice la implementación de algunos métodos de la clase.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include "Node.h"
 #include "DoublyLinkedList.h"
 #include<iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+using std::string;
 
 int main() {
     // Tu código de prueba para DoublyLinkedList aquí
-    printf("Hello, World!\n");
+    DoublyLinkedList<int> list;
+    list.insertBack(10);
+    list.insertBack(20);
+    list.insertFront(5);
+    cout << "La longitud de la lista es: " << list.getLength() << endl;
+    cout << "Elementos en la lista: ";
+    cout << list;
     return 0;
 }
