68b2c8307ee81179324d9da208eafb716fe79a40
mayra miranda moreno <mayra_190@MacBook-Air-192.local>
2025-10-04 12:50:12 -0600

    Se completó la clase Register

--- Register.cpp
+++ Register.cpp
@@ -2,6 +2,51 @@
 #include "Register.h"
 #include <sstream>
 
+// Constructores 
+
+Register::Register(): ip(IP()), falla(""), fecha(""){}
+
+Register::Register(IP _ip, string _falla, string _fecha){
+    ip = _ip;
+    falla = _falla;
+    fecha = _fecha;
+}
+
+~Register(){
+    ip = IP();
+    falla = "";
+    fecha = "";
+}
+
+// Getters
+
+string Register::get_iP(){
+    return ip.original();
+}
+
+string Register::get_falla(){
+    return falla;
+}
+
+string Register::get_fecha(){
+    return fecha;
+}
+
+// Setters
+
+void Register::set_iP(string _iP){
+    ip = _iP;
+}
+
+void Register::set_falla(string _falla){
+    falla = _falla;
+}
+
+void Register::set_fecha(string _fecha){
+    fecha = _fecha;
+}
+
+// Métodos estáticos
 Register Register::fromLinea(const std::string& linea){
     // Realiza la lectura de la línea de texto
     stringstream iss(linea);
@@ -22,6 +67,15 @@ Register Register::fromLinea(const std::string& linea){
     }
 
     // Retorna el registro creado
-    return Register(fecha, iP, falla);
+    return Register(IP(iP), falla, fecha);
 }
 
+// Operadores de comparación por IP
+
+bool Register::operator<=(const Register& R1) const{
+    return ip <= R1.ip;
+}
+
+bool Register::operator>=(const Register& R1) const{
+    return ip >= R1.ip;
+}
